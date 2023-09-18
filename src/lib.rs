1d339755407d283fb0cd0e820133a24d420d2196
shroomwastaken <xeoplay123456@gmail.com>
2023-09-18 19:27:41 +0300

    refactored + added getting map name

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,39 @@
 use hex;
 
-// takes a string of hex bytes (decodestr); returns readable string (e.g. "484c3244454d4f" into "HL2DEMO")
+// takes a string of hex bytes (decodestr);
+// returns readable string (e.g. "484c3244454d4f" into "HL2DEMO");
 fn decoder(decodestr: &String) -> String {
     return String::from_utf8(hex::decode(decodestr).unwrap()).unwrap();
 }
 
-// takes file bytes (contents); returns true if the first 7 bytes == "HL2DEMO"
-pub fn recognize_demo(contents: &Vec<u8>) -> bool {
-    let mut decodestr: String = "".to_string();
+// takes file bytes (contents), start index (start), end index (end);
+// returns String-ified bytes (every non-null character from contents[start] to contents[end] turned into a String);
+fn get_byte_range_into_string(contents: &Vec<u8>, start: usize, end: usize) -> String  {
+    let mut decode_str: String = "".to_string();
     
-    for i in  0..7 {
-        decodestr.push_str(&format!("{:x}", &contents[i]));
+    for i in  start..end + 1 {
+        if contents[i] != 0 {
+            decode_str.push_str(&format!("{:x}", &contents[i]));
+        } 
     }
 
-    if decoder(&decodestr) == "HL2DEMO" {
-        return true;
-    }
-    
-    return false;
+    return decoder(&decode_str);
 }
 
-// takes file bytes (contents); returns client name (every non-null character from 276 to 349 turned into a String)
+// takes file bytes (contents);
+// returns true if the first 7 bytes == "HL2DEMO";
+pub fn recognize_demo(contents: &Vec<u8>) -> bool {
+    return get_byte_range_into_string(contents, 0, 6) == "HL2DEMO";
+}
+
+// takes file bytes (contents);
+// returns client name (assuming client name length <= 50 characters);
 pub fn get_client_name(contents: &Vec<u8>) -> String {
-    let mut decodestr: String = "".to_string();
+    return get_byte_range_into_string(contents, 276, 326);
+}
 
-    for i in 276..350 { // assuming steam name is less than 74 characters long
-        if contents[i] != 0 {
-            decodestr.push_str(&format!("{:x}", &contents[i]));
-        }
-    }
-
-    return decoder(&decodestr);
+// takes file bytes (contents);
+// returns map name (maximum map name length is 13 characters (testchmb_a_XX));
+pub fn get_map_name(contents: &Vec<u8>) -> String {
+    return get_byte_range_into_string(contents, 536, 548);
 }
