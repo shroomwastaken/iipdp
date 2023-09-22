bb2e2656bacd90e4cf8a59606d88024d0fc6fa4e
shroomwastaken <xeoplay123456@gmail.com>
2023-09-22 16:37:34 +0300

    better printing

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,17 @@ fn main() {
         return;
     }
 
-    for (name, value) in header_info {
-        println!("{:?}: {:?}", name, value)
-    }
+    println!("File Stamp: {:?}", header_info["DemoFileStamp"]);
+    println!("Demo Protocol: {:?}", header_info["DemoProtocol"]);
+    println!("Network Protocol: {:?}", header_info["NetworkProtocol"]);
+    println!("Server Name: {:?}", header_info["ServerName"]);
+    println!("Client Name: {:?}", header_info["ClientName"]);
+    println!("Map Name: {:?}", header_info["MapName"]);
+    println!("Game Directory: {:?}", header_info["GameDirectory"]);
+    println!("Playback Time: {:?}", header_info["PlaybackTime"]);
+    println!("Playback Ticks: {:?}", header_info["PlaybackTicks"]);
+    println!("Playback Frames: {:?}", header_info["PlaybackFrames"]);
+    println!("Sign On Length: {:?}", header_info["SignOnLength"]);
 
     io::stdin().read_line(&mut String::new()).unwrap();
 }
