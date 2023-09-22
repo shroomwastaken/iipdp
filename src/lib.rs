3919f99aa103f3775eb94605e5555fe26e9368e3
shroomwastaken <xeoplay123456@gmail.com>
2023-09-22 10:07:14 +0300

    working header info parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,76 @@
+use std::collections::HashMap;
+use std::i32;
+use std::u32;
+use unicode_segmentation::UnicodeSegmentation;
 use hex;
 
-// takes a string of hex bytes (decodestr);
+/*
+    WARNING: BEAUTIFUL CODE AHEAD
+*/
+
+// takes a string of hex bytes (decodestr), value type (value_type);
 // returns readable string (e.g. "484c3244454d4f" into "HL2DEMO");
-fn decoder(decodestr: &String) -> String {
-    return String::from_utf8(hex::decode(decodestr).unwrap()).unwrap();
+fn decoder(decode_str: &String, value_type: &str) -> String {
+    if value_type == "string" {
+        return String::from_utf8_lossy(&hex::decode(&decode_str).unwrap()).to_string();
+    } else if value_type == "int" {
+        if decode_str.contains("ffffff") {
+            let byte_slice = &decode_str
+                .as_bytes()
+                .chunks(2)
+                .map(|chunk| u8::from_str_radix(std::str::from_utf8(chunk).unwrap(), 16).unwrap())
+                .collect::<Vec<u8>>();
+            let mut le_byte_slice = [0; 4];
+            le_byte_slice.copy_from_slice(&byte_slice);
+            return i32::from_le_bytes([le_byte_slice[0], le_byte_slice[1], le_byte_slice[2], le_byte_slice[3]]).to_string();
+        } else {
+            let byte_slice = hex::decode(&decode_str).unwrap();
+            return i32::from_le_bytes([byte_slice[0], byte_slice[1], byte_slice[2], byte_slice[3]]).to_string();
+        }
+        
+    } else if value_type == "float" {
+        let byte_slice = hex::decode(&decode_str).unwrap();
+        return f32::from_bits(u32::from_le_bytes([byte_slice[0], byte_slice[1], byte_slice[2], byte_slice[3]])).to_string();
+    }
+    return "something went horribly wrong".to_string();
 }
 
 // takes file bytes (contents), start index (start), end index (end);
 // returns String-ified bytes (every non-null character from contents[start] to contents[end] turned into a String);
-fn get_byte_range_into_string(contents: &Vec<u8>, start: usize, end: usize) -> String  {
+fn get_byte_range_into_string(contents: &Vec<u8>, start: usize, length: usize, value_type: &str) -> String  {
     let mut decode_str: String = "".to_string();
     
-    for i in  start..end + 1 {
-        if contents[i] != 0 {
+    for i in start..start + length {
+        if format!("{:x}", contents[i]).graphemes(true).count()== 1 {
+            decode_str.push_str(&("0".to_owned() + &format!("{:x}", &contents[i])));
+        } else if format!("{:x}", contents[i]).graphemes(true).count() == 2 {
             decode_str.push_str(&format!("{:x}", &contents[i]));
-        } 
+        }
     }
 
-    return decoder(&decode_str);
+    return decoder(&decode_str, value_type).trim_end_matches("\0").to_string();
 }
 
 // takes file bytes (contents);
 // returns true if the first 7 bytes == "HL2DEMO";
 pub fn recognize_demo(contents: &Vec<u8>) -> bool {
-    return get_byte_range_into_string(contents, 0, 6) == "HL2DEMO";
+    return get_byte_range_into_string(contents, 0, 8, "string") == "HL2DEMO";
 }
 
-// takes file bytes (contents);
-// returns client name (assuming client name length <= 50 characters);
-pub fn get_client_name(contents: &Vec<u8>) -> String {
-    return get_byte_range_into_string(contents, 276, 326);
-}
+pub fn get_header_info(contents: &Vec<u8>) -> HashMap<&str, String> {
+    let mut header_info: HashMap<&str, String> = HashMap::new();
 
-// takes file bytes (contents);
-// returns map name (maximum map name length is 13 characters (testchmb_a_XX));
-pub fn get_map_name(contents: &Vec<u8>) -> String {
-    return get_byte_range_into_string(contents, 536, 548);
+    header_info.insert("DemoFileStamp", get_byte_range_into_string(contents, 0, 8, "string"));
+    header_info.insert("DemoProtocol", get_byte_range_into_string(contents, 8, 4, "int"));
+    header_info.insert("NetworkProtocol", get_byte_range_into_string(contents, 12, 4, "int"));
+    header_info.insert("ServerName", get_byte_range_into_string(contents, 16, 260, "string"));
+    header_info.insert("ClientName", get_byte_range_into_string(contents, 276, 260, "string"));
+    header_info.insert("MapName", get_byte_range_into_string(contents, 536, 260, "string"));
+    header_info.insert("GameDirectory", get_byte_range_into_string(contents, 796, 260, "string"));
+    header_info.insert("PlaybackTime", get_byte_range_into_string(contents, 1056, 4, "float"));
+    header_info.insert("PlaybackTicks", get_byte_range_into_string(contents, 1060, 4, "int"));
+    header_info.insert("PlaybackFrames", get_byte_range_into_string(contents, 1064, 4, "int"));
+    header_info.insert("SignOnLength", get_byte_range_into_string(contents, 1068, 4, "int"));
+
+    return header_info;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ use iipdp;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("you didnt provide a demo file");
+        io::stdin().read_line(&mut String::new()).unwrap();
+        return;
+    }
     
-    let contents = fs::read(&args[1]).expect("hi");
+    let contents = fs::read(&args[1]).expect(r#"demo file reading failed ¯\_(ツ)_/¯"#);
+
+    let header_info = iipdp::get_header_info(&contents);
 
     if iipdp::recognize_demo(&contents) {
         println!("demo recognized!");
@@ -16,7 +24,9 @@ fn main() {
         return;
     }
 
-    println!("{:?}", iipdp::get_client_name(&contents));
+    for (name, value) in header_info {
+        println!("{:?}: {:?}", name, value)
+    }
 
     io::stdin().read_line(&mut String::new()).unwrap();
 }
