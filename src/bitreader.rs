e386008a8db12fee3f997c7e690cea1202396308
shroomwastaken <xeoplay123456@gmail.com>
2023-11-07 14:48:46 +0300

    made bitreader go very fast

--- src/bitreader.rs
+++ src/bitreader.rs
@@ -1,102 +1,147 @@
 use crate::structs::utils::{Vec3, EHandle};
 
+// the struct and the read_bits, fetch and skip functions are from https://github.com/evanlin96069/sdp-c/blob/master/src/bits.c
+// huge thanks to evanlin96069 for giving advice and making readable c code
+
+#[derive(Debug, Clone)]
 pub struct BitReader {
-    pub contents: Vec<u8>,
-    pub cur_bit_index: i32,
+    pub bits: Vec<u8>,
+    pub offset: u8,
+    pub bit_size: usize,
+    pub byte_size: usize,
+    pub fetch: u64,
+    pub current: usize,
 }
 
 impl BitReader {
-    // this is pretty unoptimized, only reads one bit at a time
-    // todo make this better by fetching a u64 at a time and shifting that or somehing idk
-    pub fn read_bits(&mut self, num_bits: i32) -> Vec<u8> {
-        let mut result:  Vec<u8> = Vec::new();
-        let mut bits_left = num_bits;
-        for _ in self.cur_bit_index / 8..self.cur_bit_index / 8 + (num_bits / 8 + if num_bits % 8 == 0 && num_bits >= 8 { 0 } else { 1 }) {
-            let mut cur_val: u8 = 0;
-            for bit in 0..if bits_left >= 8 { 8 } else { bits_left } {
-                cur_val |= ((self.contents[(self.cur_bit_index / 8) as usize] >> ((self.cur_bit_index) % 8)) & 1) << bit;
-                self.cur_bit_index += 1;
-            }
-            result.push(cur_val);
-            bits_left -= 8;
+    pub fn new(bits: Vec<u8>) -> BitReader{
+        let mut new_reader = BitReader {
+            bits: Vec::new(),
+            offset: 0,
+            bit_size: 0,
+            byte_size: 0,
+            fetch: 0,
+            current: 0,
+        };
+        new_reader.bits = bits;
+        new_reader.bit_size = new_reader.bits.len() * 8;
+        new_reader.byte_size = new_reader.bit_size / 8;
+        new_reader.fetch();
+        
+        return new_reader;
+    }
+    
+    pub fn fetch(&mut self) {
+        let block = if self.current / 8 + 8 > self.byte_size { self.byte_size - 8 } else { self.current / 8 };
+        self.fetch = u64::from_le_bytes(self.bits[block..block + 8].try_into().unwrap());
+        self.offset = (self.current - (block * 8)) as u8;
+    }
+        
+    // reads bits
+    pub fn read_bits(&mut self, amount: i32) -> u64 {        
+        if self.current + amount as usize > self.bit_size {
+            panic!("overflowed somehow lmao");
         }
-        return result;
+        let mut new_amount = amount.to_owned() as usize; // this is so that i can modify the amount value
+        let mut res: u64 = 0;
+        let remain: usize = (64 - self.offset).into(); // bits remaining in the current fetch
+        let mut shift: usize = 0; // uhhhhh
+        
+
+        if remain == 0 { // if we've gone through the entire fetch
+            self.fetch();
+        } else if new_amount > remain { // if we havent gone trough the entire fetch yet but we need to get the next one
+            res |= (self.fetch >> self.offset) & ((1u64 << remain) - 1);
+            self.skip(remain as i32); // add remain bits to the current bit index, get new fetch (see skip function)
+            new_amount -= remain; // how many bits are left to read
+            shift = remain; // what position to put those bits in
+        }
+
+        /*
+            EXAMPLE:
+
+            fetch = 0b0111000000010100110100000000000000000000010001101001000011000000
+            offset = 6
+            amount = 8
+            remain = 58
+            shift: 0
+
+            fetch >> offset                             = 0b0000000111000000010100110100000000000000000000010001101001000011
+            (1u64 << amount) - 1                        = 0b0000000000000000000000000000000000000000000000000000000011111111
+            (fetch >> offset) & ((1u64 << amount) - 1)  = 0b0000000000000000000000000000000000000000000000000000000001000011
+            
+            then we right shift it by "shift" to put the bits into their correct place in the number if we fetched above
+        */
+
+        res |= ((self.fetch >> self.offset) & ((1u64 << new_amount) - 1)) << shift;
+        
+        self.current += new_amount;
+        self.offset += new_amount as u8;
+        return res;
+    }
+    
+    // do i have to explain this one
+    pub fn skip(&mut self, amount: i32) {
+        self.current += amount as usize;
+        if self.current > self.bit_size {
+            panic!("overflow while skipping");
+        }
+        self.fetch();
+    }
+
+    // reads character by character until the character is \0
+    pub fn read_ascii_string_nulled(&mut self) -> String {
+        let mut char_vec: Vec<u8> = Vec::new();
+        let mut cur_char = self.read_bits(8);
+        while cur_char != 0 {
+            char_vec.push(cur_char as u8);
+            cur_char = self.read_bits(8);
+        }
+
+        return String::from_utf8(char_vec).unwrap().trim_end_matches("\0").to_string();
     }
 
     // clones the bitreader and skips amount bits in the parent one
     pub fn split_and_skip(&mut self, amount: i32) -> Self {
-        let new_reader = BitReader { contents: self.contents.clone(), cur_bit_index: self.cur_bit_index };
+        let new_reader = self.clone();
 
         self.skip(amount);
 
         return new_reader;
     }
 
-    // self-explanantory
-    pub fn skip(&mut self, amount: i32) {
-        self.cur_bit_index += amount;
-    }
-
-    // could maybe be better but whatever
+    // reads an int ¯\_(ツ)_/¯
     pub fn read_int(&mut self, amount: i32) -> i32 {
-        let bytes = self.read_bits(amount);
-        return match bytes.len() {
-            1 => i32::from_le_bytes([bytes[0], 0, 0, 0]),
-            2 => i32::from_le_bytes([bytes[0], bytes[1], 0, 0]),
-            3 => i32::from_le_bytes([bytes[0], bytes[1], bytes[2], 0]),
-            4 => i32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
-            _ => 0,
-        }
+        return i32::from_le_bytes((self.read_bits(amount) as u32).to_le_bytes());
     }
 
     // used once, dont remember where
     pub fn read_uint_64(&mut self) -> u64 {
-        return u64::from_le_bytes(self.read_bits(64).as_slice().try_into().unwrap());
+        return u64::from_le_bytes(self.read_bits(64).to_le_bytes());
     }
 
     // same as read_int function but float :0
     pub fn read_float(&mut self, amount: i32) -> f32 {
-        let bytes = self.read_bits(amount);
-        return (match bytes.len() {
-            1 => f32::from_le_bytes([bytes[0], 0, 0, 0]),
-            2 => f32::from_le_bytes([bytes[0], bytes[1], 0, 0]),
-            3 => f32::from_le_bytes([bytes[0], bytes[1], bytes[2], 0]),
-            4 => f32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
-            _ => 0.0,
-        } * 1000.0).round() / 1000.0
-    }
-
-    // read ascii string without a determined length,
-    // all(?) ascii strings in the demo are null terminated
-    pub fn read_ascii_string_nulled(&mut self) -> String {
-        let mut char_vec: Vec<u8> = Vec::new();
-        let mut byte = self.read_bits(8)[0];
-
-        // while the next character isnt a null terminator ("\0")
-        while byte != 0 {
-            char_vec.push(byte);
-            byte = self.read_bits(8)[0];
-        }
-
-        // account for the null terminator
-        char_vec.push(byte);
-
-        let res = String::from_utf8(char_vec).unwrap().trim_end_matches("\0").to_string();
-
-        return res;
+        return f32::from_le_bytes((self.read_bits(amount) as u32).to_le_bytes());
     }
 
     // read ascii string that has a determined length
     // used mostly in the header
     pub fn read_ascii_string(&mut self, amount: i32) -> String {
-        return String::from_utf8(self.read_bits(amount)).unwrap().trim_end_matches("\0").to_string();
+        let mut char_vec: Vec<u8> = Vec::new();
+        for _ in 0..amount / 8 {
+            let cur_char = self.read_bits(8);
+            char_vec.push(cur_char as u8);
+        }
+
+        return String::from_utf8(char_vec).unwrap().trim_end_matches("\0").to_string();
     }
 
     // also used once
     pub fn read_bytes(&mut self, amount: i32) -> Vec<u8> {
         let mut res: Vec<u8> = Vec::new();
         for _ in 0..amount {
-            let cur_byte = self.read_bits(8)[0];
+            let cur_byte = self.read_bits(8) as u8;
             res.push(cur_byte);
         }
 
@@ -159,7 +204,7 @@ impl BitReader {
     pub fn read_ehandle(&mut self) -> EHandle { return EHandle { val: self.read_int(32)} }
 
     // do i need to explain this
-    pub fn read_bool(&mut self) -> bool { return self.read_bits(1)[0] == 1; }
+    pub fn read_bool(&mut self) -> bool { return self.read_bits(1) == 1; }
 
     // the read_x_if_exists functions only read amount bits if the next bit is set
     pub fn read_int_if_exists(&mut self, amount: i32) -> Option<i32> { return if self.read_bool() { Some(self.read_int(amount)) } else { None } }
